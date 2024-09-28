b62881faeab831e9bcfd53cb34dbd82930079309
Olivia <98853542+oliviamrkw@users.noreply.github.com>
2024-09-28 01:07:10 -0400

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -29,59 +29,23 @@ Dear {receiver_name},\n
 
         bottom_text = """
 \n
-Kind Regards,
-
-
-Client Care Team
-
-Kristin L., Sr. Tax Consultant
-
-Tax 911 NOW! -  Affordable Experts Who Fix Urgent Tax Problems
-35 West Pearce St., Unit 19
-Richmond Hill, ON, L4B 3A9
-Phone:  1-877-918-2991
-Fax:      1-888-835-4943
-Email: Help@Tax911NOW.ca
+Your signature here.
         """
         
         first_followup_text = f"""  
-Thank you for contacting Tax 911 Now!, the Urgent Tax Problem Solver. Congratulations on taking the action to clear up the tax problems.
-
-Do you know we offer the first consultation for free?
-
-In the consultation, we will review all aspects of your tax situation and provide you with the best course of action to solve your tax problems fast.
-
-Then it is completely up to you to decide what to do next.
-
-Please call us at 1-877-918-2991 or 416-840-6899 between 10:00am - 6:00pm Monday to Friday or email to Help@Tax911Now.ca to book your first appointment.
-
-We will also try to contact you shortly.
+Your text here.
         """
 
         second_followup_text = f"""
-Regarding your tax issue, we have just called and left a message asking for a callback.
-
-You can also call us again to book your free no obligation consultation to learn about the best solutions to solve your tax problems fast.
-
-Please call us at 1-877-918-2991 between 10:00am - 6:00pm Monday to Friday or email to Help@Tax911Now.ca to book your first appointment.
-
-We will also try to contact you again shortly.
+Your text here.
         """
 
         third_followup_text = f"""
-Just to follow up with you again to find out if you have any questions about the tax issue. We will be more than happy to assist you with further questions.
-
-You can call us at 1-877-918-2991 between 10:00am - 6:00pm Monday to Friday or email Help@Tax911Now.ca.
+Your text here.
         """
 
         fourth_followup_text = f"""
-Just wanted to follow up with you regarding your tax issue. Have you made any further progress yet?
-
-Please let us know if you need any help moving forward. What we do every day is to help people like yourself move on with their lives without a tax burden.
-
-Remember, Tax Problems won't go away unless you take action!
-
-You can call us at 1-877-918-2991 between 10:00am - 6:00pm Monday to Friday or email Help@Tax911Now.ca if you have any questions.
+Your text here.
         """
 
         if followup_index.lower() == "first" or followup_index == "1":
